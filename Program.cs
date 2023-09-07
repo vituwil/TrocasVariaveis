f8b8470a25a3637a0bc0585009d3d425edb88a13
Victor <victorrefundine@hotmail.com>
2023-09-06 21:02:27 -0300

    Adição do programa

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string nome1, nome2, auxiliar;
+
+Console.WriteLine(" Troca de Valores ");
+Console.WriteLine();
+
+Console.Write("Nome 1: ");
+nome1 = Console.ReadLine()!;
+
+Console.Write("Nome 2: ");
+nome2 = Console.ReadLine()!;
+
+Console.WriteLine();
+
+auxiliar = nome1;
+nome1 = nome2;
+nome2 = auxiliar;
+
+Console.WriteLine("Após trocar os valores temos:");
+Console.WriteLine();
+
+Console.WriteLine($"Nome 1 = {nome1}");
+Console.WriteLine($"Nome 2 = {nome2}");
